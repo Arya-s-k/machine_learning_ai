0097e112f494ae82a2f488d1c60545dfdec1dcf4
Arya-s-k <nayana.shashi@gmail.com>
2025-10-30 20:04:21 +0530

    Add files via upload

---
+++ acp/acp1.sql
@@ -0,0 +1,9 @@
+create table sales (sno integer,name text, sales integer,city text);
+
+insert into sales (sno,name,sales,city) values ("s01","ram",10,"Bangalore"),
+("s02","shyam",20,"new york"),
+("s03","risha",30,"mumbai"),
+("s04","sammmy",10,"london"),
+("s05","rakesh",10,"houston");
+
+select * from sales
